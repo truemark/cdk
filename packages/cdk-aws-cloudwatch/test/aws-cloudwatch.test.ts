661086f30ff37ff5b6634172344f08680202a92e
Erik Jensen <ejensen@truemark.io>
2022-05-10 19:42:35 -0600

    Removing old packages

--- packages/cdk-aws-cloudwatch/test/aws-cloudwatch.test.ts
+++
@@ -1,6 +0,0 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as AwsCloudwatch from '../lib/index';
-test('LogAlarm Created', () => {
-  // TODO
-});
