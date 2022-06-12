90dcaadb925358ffeefe0923ed34f107b6c32296
Erik Jensen <ejensen@truemark.io>
2022-06-12 17:33:36 -0600

    Adding ParameterStore

--- packages/truemark-cdk-lib/aws-ssm/lib/index.ts
+++ packages/truemark-cdk-lib/aws-ssm/lib/index.ts
@@ -1,1 +1,2 @@
 export * from "./parameter-reader";
+export * from "./parameter-store";
---
+++ packages/truemark-cdk-lib/aws-ssm/lib/parameter-store.ts
@@ -0,0 +1,83 @@
+import {Stack} from "aws-cdk-lib";
+import {Construct} from "constructs";
+import {StringParameter} from "aws-cdk-lib/aws-ssm";
+import {ParameterReader} from "./parameter-reader";
+
+/**
+ * Properties for ParameterStore
+ */
+export interface ParameterStoreProps {
+  readonly prefix?: string;
+  readonly suffix?: string;
+  readonly region?: string;
+}
+
+/**
+ * Utility construct to ease reading and writing parameters with cross-region read support.
+ */
+export class ParameterStore extends Construct {
+
+  /**
+   * The region in which to access the parameters.
+   */
+  readonly region: string;
+
+  /**
+   * The prefix on parameters read and written by this store.
+   */
+  readonly prefix: string;
+
+  /**
+   * The suffix on parameters read and written by this store.
+   */
+  readonly suffix: string;
+
+  constructor(scope: Construct, id: string, props?: ParameterStoreProps) {
+    super(scope, id);
+    this.prefix = props?.prefix??"";
+    this.suffix = props?.suffix??"";
+    this.region = props?.region??Stack.of(this).region;
+  }
+
+  regionMatch(): boolean {
+    return this.region === Stack.of(this).region;
+  }
+
+  /**
+   * Creates a new SSM parameter in the local region.
+   *
+   * @param name the name of the parameter
+   * @param value the value to store
+   */
+  write(name: string, value: string): StringParameter {
+    if (!this.regionMatch()) {
+      throw new Error("Cannot write to a different region");
+    }
+    return new StringParameter(this, name, {
+      parameterName: this.prefix + name + this.suffix,
+      stringValue: value
+    });
+  }
+
+  protected readLocal(name: string): string {
+    return StringParameter.fromStringParameterAttributes(this, name, {
+      parameterName: this.prefix + name + this.suffix
+    }).stringValue;
+  }
+
+  protected readRemote(name: string): string {
+    return new ParameterReader(this, name, {
+      parameterName: this.prefix + name + this.suffix,
+      region: this.region
+    }).getStringValue();
+  }
+
+  /**
+   * Reads an SSM parameter from a local or remote region.
+   *
+   * @param name the name of the parameter to read
+   */
+  read(name: string): string {
+    return this.regionMatch() ? this.readLocal(name) : this.readRemote(name);
+  }
+}
