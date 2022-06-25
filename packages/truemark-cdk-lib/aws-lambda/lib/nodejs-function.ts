ba1291f385188dac1d66843a298806b89d912313
Erik Jensen <ejensen@truemark.io>
2022-06-24 18:28:30 -0600

    Adding debug

--- packages/truemark-cdk-lib/aws-lambda/lib/nodejs-function.ts
+++ packages/truemark-cdk-lib/aws-lambda/lib/nodejs-function.ts
@@ -45,7 +45,8 @@ export class NodejsFunction extends nodejs.NodejsFunction {
         commandHooks: {
           beforeBundling(inputDir: string, outputDir: string): string[] {
             return [
-              `if [ -f ${inputDir}/package-lock.json ]; then npm ci -p ${inputDir} --prefer-offline --no-fund; fi`
+              `ls -l ${inputDir}`,
+              `if [ -f ${inputDir}/package-lock.json ]; then npm ci --prefix ./${inputDir} --prefer-offline --no-fund; fi`
             ]
           },
           beforeInstall(inputDir: string, outputDir: string): string[] {
