8476b2fcc8b3c0a87e32c51301baf2b2c73918dc
Erik Jensen <ejensen@truemark.io>
2022-09-01 05:40:46 -0600

    Adding ParameterPaths class

---
+++ packages/truemark-cdk-lib/aws-ssm/lib/parameter-paths.ts
@@ -0,0 +1,60 @@
+import {Grant, IGrantable} from "aws-cdk-lib/aws-iam";
+import {Stack} from "aws-cdk-lib";
+import {Construct} from "constructs";
+
+/**
+ * Represents a group of SSM Parameter paths.
+ */
+export class ParameterPaths {
+
+  private readonly stack: Stack;
+  readonly paths: string[];
+
+  constructor(scope: Construct, ...paths: string[]) {
+    this.stack = Stack.of(scope);
+    this.paths = paths;
+  }
+
+  grantRead(grantee: IGrantable): Grant {
+    return Grant.addToPrincipal({
+      grantee,
+      actions: [
+        "ssm:GetParameter",
+        "ssm:DescribeParameters",
+        "ssm:GetParameters",
+        "ssm:GetParametersByPath",
+        "ssm:GetParameterHistory",
+      ],
+      resourceArns: this.paths.map(path => `"arn:aws:ssm:${this.stack.region}:${this.stack.account}:parameter/${path}`)
+    });
+  }
+
+  grantWrite(grantee: IGrantable): Grant {
+    return Grant.addToPrincipal({
+      grantee,
+      actions: [
+        "ssm:PutParameter",
+        "ssm:DeleteParameter",
+        "ssm:DeleteParameters",
+      ],
+      resourceArns: this.paths.map(path => `"arn:aws:ssm:${this.stack.region}:${this.stack.account}:parameter/${path}`)
+    })
+  }
+
+  grantReadWrite(grantee: IGrantable): Grant {
+    return Grant.addToPrincipal({
+      grantee,
+      actions: [
+        "ssm:GetParameter",
+        "ssm:DescribeParameters",
+        "ssm:GetParameters",
+        "ssm:GetParametersByPath",
+        "ssm:GetParameterHistory",
+        "ssm:PutParameter",
+        "ssm:DeleteParameter",
+        "ssm:DeleteParameters"
+      ],
+      resourceArns: this.paths.map(path => `"arn:aws:ssm:${this.stack.region}:${this.stack.account}:parameter/${path}`)
+    })
+  }
+}
