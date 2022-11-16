6bb02f078352ea2f5b46e711d81f9700d55cb7d7
Erik Jensen <ejensen@truemark.io>
2022-11-16 06:24:45 -0700

    Adding helper methods to handle case conversions

--- packages/truemark-cdk-lib/helpers/lib/string-helper.ts
+++ packages/truemark-cdk-lib/helpers/lib/string-helper.ts
@@ -1,17 +1,69 @@
+/**
+ * Helper methods for use on Strings.
+ */
 export class StringHelper {
 
+  /**
+   * Tests if a string is PascalCase.
+   *
+   * @param str the string to test
+   */
   static isPascalCase(str?: string | null) {
     if (str) {
-      return /^[A-Z][a-z]+(?:[A-Z][a-z0-9]+)*$/.test(str);
+      return /^[A-Z](([a-z0-9]+[A-Z]?)*)$/.test(str);
     }
     return false;
   }
 
+  /**
+   * Tests if a string is camelCase.
+   *
+   * @param str the string to test
+   */
   static isCamelCase(str?: string | null) {
     if (str) {
-      return /^[a-z]+(?:[A-Z][a-z0-9]+)*$/.test(str);
+      return /^[a-z][a-z0-9]*(([A-Z][a-z0-9]+)*[A-Z]?|([a-z0-9]+[A-Z])*|[A-Z])$/.test(str);
     }
     return false;
+  }
+
+  /**
+   * Converts a string to snake_case
+   *
+   * @param str the string to convert
+   */
+  static toSnakeCase(str: string): string {
+    return (str.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g) || [])
+      .map(x => x.toLowerCase()).join("_");
+  }
+
+  /**
+   * Converts a string to kebab-case
+   *
+   * @param str the string to convert
+   */
+  static toKebabCase(str: string): string {
+    return (str.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g) || [])
+      .map(x => x.toLowerCase()).join("-");
+  }
+
+  /**
+   * Converts a string to camelCase.
+   *
+   * @param str the string to convert
+   */
+  static toCamelCase(str: string): string {
+    str = this.toPascalCase(str);
+    return str.charAt(0).toLowerCase() + str.slice(1);
+  }
+
+  /**
+   * Converts a string to PascalCase.
+   *
+   * @param str the string to convert
+   */
+  static toPascalCase(str: string): string {
+    return (str.match(/[a-zA-Z0-9]+/g) || []).map(x => `${x.charAt(0).toUpperCase()}${x.slice(1)}`).join("");
   }
 
 }
--- packages/truemark-cdk-lib/test/helpers/string-helper.test.ts
+++ packages/truemark-cdk-lib/test/helpers/string-helper.test.ts
@@ -17,3 +17,31 @@ test("Test StringHelper#isPascalCase", () => {
   expect(StringHelper.isPascalCase("testThis")).toBe(false);
   expect(StringHelper.isPascalCase("Test-This")).toBe(false);
 });
+
+test("Test StringHelper.toSnakeCase", () => {
+  expect(StringHelper.toSnakeCase("ThisIsATest")).toBe("this_is_a_test");
+  expect(StringHelper.toSnakeCase("This_Is_A_Test")).toBe("this_is_a_test");
+  expect(StringHelper.toSnakeCase("This-Is-A-Test")).toBe("this_is_a_test");
+  expect(StringHelper.toSnakeCase("This Is A Test")).toBe("this_is_a_test");
+});
+
+test("Test StringHelper.toKebabCase", () => {
+  expect(StringHelper.toKebabCase("ThisIsATest")).toBe("this-is-a-test");
+  expect(StringHelper.toKebabCase("This_Is_A_Test")).toBe("this-is-a-test");
+  expect(StringHelper.toKebabCase("This-Is-A-Test")).toBe("this-is-a-test");
+  expect(StringHelper.toKebabCase("This Is A Test")).toBe("this-is-a-test");
+});
+
+test("Test StringHelper.toCamelCase", () => {
+  expect(StringHelper.toCamelCase("this-is-a-test")).toBe("thisIsATest");
+  expect(StringHelper.toCamelCase("this_is_a_Test")).toBe("thisIsATest");
+  expect(StringHelper.toCamelCase("ThisIsATest")).toBe("thisIsATest");
+  expect(StringHelper.toCamelCase("This Is A Test")).toBe("thisIsATest");
+});
+
+test("Test StringHelper.toPascalCase", () => {
+  expect(StringHelper.toPascalCase("this_is_a_test")).toBe("ThisIsATest");
+  expect(StringHelper.toPascalCase("this_is_a_Test")).toBe("ThisIsATest");
+  expect(StringHelper.toPascalCase("ThisIsATest")).toBe("ThisIsATest");
+  expect(StringHelper.toPascalCase("this is a test")).toBe("ThisIsATest");
+});
