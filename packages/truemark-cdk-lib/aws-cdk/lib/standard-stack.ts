0a1574c2485572cbc443f903ae36052dae871f03
Erik Jensen <ejensen@truemark.io>
2022-12-03 12:46:46 -0700

    Making props not requires for StandardStack

--- packages/truemark-cdk-lib/aws-cdk/lib/standard-stack.ts
+++ packages/truemark-cdk-lib/aws-cdk/lib/standard-stack.ts
@@ -1,8 +1,7 @@
 import {ExtendedStack, ExtendedStackProps} from "./extended-stack";
 import {StandardApp} from "./standard-app";
 import {StandardStage} from "./standard-stage";
-import {StandardEnvironment} from "./standard-environment";
-import {CDK_NPMJS_URL, CDK_VENDOR, StringHelper} from "../../helpers";
+import {CDK_NPMJS_URL, CDK_VENDOR} from "../../helpers";
 import {
   AutomationTagsProps,
   CostCenterTagsProps,
@@ -31,28 +30,28 @@ export class StandardStack extends ExtendedStack {
   readonly teamTags?: TeamTagsProps;
   readonly suppressTags?: boolean;
 
-  constructor(scope: Construct, id: string, props: StandardStackProps) {
+  constructor(scope: Construct, id: string, props?: StandardStackProps) {
     super(scope, id, props);
 
     const standardScope: StandardApp | StandardStage | StandardStack | undefined =
       scope instanceof StandardApp || scope instanceof StandardStage || scope instanceof StandardStack
         ? scope : undefined;
 
-    this.automationTags = props.automationTags ?? standardScope?.automationTags;
-    this.costCenterTags = props.costCenterTags ?? standardScope?.costCenterTags;
-    this.securityTags = props.securityTags ?? standardScope?.securityTags;
-    this.teamTags = props.teamTags ?? standardScope?.teamTags;
-    this.suppressTags = props.suppressTags ?? standardScope?.suppressTags;
+    this.automationTags = props?.automationTags ?? standardScope?.automationTags;
+    this.costCenterTags = props?.costCenterTags ?? standardScope?.costCenterTags;
+    this.securityTags = props?.securityTags ?? standardScope?.securityTags;
+    this.teamTags = props?.teamTags ?? standardScope?.teamTags;
+    this.suppressTags = props?.suppressTags ?? standardScope?.suppressTags;
 
-    new StandardTags(this, {suppress: props.suppressTags})
+    new StandardTags(this, {suppress: props?.suppressTags})
       .addAutomationComponentTags({
         url: CDK_NPMJS_URL,
         vendor: CDK_VENDOR,
         includeResourceTypes: ["AWS::CloudFormation::Stack"]
       })
-      .addAutomationTags(props.automationTags)
-      .addCostCenterTags(props.costCenterTags)
-      .addSecurityTags(props.securityTags)
-      .addTeamTags(props.teamTags);
+      .addAutomationTags(props?.automationTags)
+      .addCostCenterTags(props?.costCenterTags)
+      .addSecurityTags(props?.securityTags)
+      .addTeamTags(props?.teamTags);
   }
 }
