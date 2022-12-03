968d64e9e7c3bf9870e8aa4c33867d0c5a598caf
Erik Jensen <ejensen@truemark.io>
2022-12-02 20:36:47 -0700

    Adding default account and region to StandardApp

--- packages/truemark-cdk-lib/aws-cdk/lib/standard-app.ts
+++ packages/truemark-cdk-lib/aws-cdk/lib/standard-app.ts
@@ -19,6 +19,8 @@ export interface StandardAppProps extends AppProps, StandardTagsOptions {
  */
 export class StandardApp extends App {
 
+  readonly account: string;
+  readonly region: string;
   readonly environmentName: string;
   readonly automationTags?: AutomationTagsProps;
   readonly costCenterTags?: CostCenterTagsProps;
@@ -28,6 +30,18 @@ export class StandardApp extends App {
 
   constructor(props?: StandardAppProps) {
     super(props);
+
+    const account = process.env.CDK_DEFAULT_ACCOUNT || process.env.AWS_ACCOUNT_ID;
+    if (account === undefined) {
+      throw new Error("Unable to identify default account");
+    }
+    this.account = account;
+    const region = process.env.CDK_DEFAULT_REGION || process.env.AWS_DEFAULT_REGION;
+    if (region === undefined) {
+      throw new Error("Unable to identify default region");
+    }
+    this.region = region;
+
     this.automationTags = props?.automationTags;
     this.costCenterTags = props?.costCenterTags;
     this.securityTags = props?.securityTags;
