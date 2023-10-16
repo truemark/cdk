ece1fadf297114988f362dd5c721aae6b4bb389d
Erik Jensen <365211+erikrj@users.noreply.github.com>
2023-10-16 15:27:14 -0700

    Fixing accidental export

--- packages/truemark-cdk-lib/aws-codepipeline/lib/index.ts
+++ packages/truemark-cdk-lib/aws-codepipeline/lib/index.ts
@@ -1,5 +1,4 @@
 export * from "./artifact-bucket";
 export * from "./cdk-pipeline";
-export * from "../../aws-cdk/lib/extended-stack";
 export * from "./pipeline-notification-rule"
 export * from "./enums"
