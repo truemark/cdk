659cca886f0f71c94b289701ffc243d2eb4981ab
Erik Jensen <365211+erikrj@users.noreply.github.com>
2023-11-13 09:19:05 -0700

    Adding construct for RDS

---
+++ packages/truemark-cdk-lib/aws-rds/lib/postgresql-aurora-serverless-v2.ts
@@ -0,0 +1,14 @@
+import {ExtendedConstruct} from "../../aws-cdk";
+
+export interface PostgreSQLAuroraServerlessV2Props {
+
+}
+
+export class PostgreSQLAuroraServerlessV2 extends ExtendedConstruct {
+  constructor(scope: ExtendedConstruct, id: string, props: PostgreSQLAuroraServerlessV2Props) {
+    super(scope, id);
+
+    // Add stuff here
+
+  }
+}
