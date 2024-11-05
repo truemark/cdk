673988ba036554874a50f878ef0889e6b2b7bfe8
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-11-04 19:40:00 -0700

    Adding CollectionType

--- aws-opensearchserverless/lib/collection.ts
+++ aws-opensearchserverless/lib/collection.ts
@@ -1,7 +1,14 @@
 import {Construct} from 'constructs';
 import {ExtendedConstruct} from '../../aws-cdk';
 
+export enum CollectionType {
+  SEARCH = 'SEARCH',
+  TIMESERIES = 'TIMESERIES',
+  VECTORSEARCH = 'VECTORSEARCH',
+}
+
 export interface CollectionProps {
+  readonly type: CollectionType;
   readonly name: string;
 }
 
